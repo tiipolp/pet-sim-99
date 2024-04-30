2eb49ef313db0b05fb07a487f72296dd91c573f6
tiipolp <98067947+tiipolp@users.noreply.github.com>
2024-04-30 03:29:44 -0500

    Update arcade.lua

--- arcade.lua
+++ arcade.lua
@@ -1,9 +1,6 @@
 local blue = "rbxassetid://13987312142"
 local pink = "rbxassetid://13987314678"
 
-local onlypink = true
-local onlyblue = false
-
 local valids = {}
 
 local function hop()
@@ -55,6 +52,5 @@ for key, value in pairs(valids) do
 end
 
 if next(valids) == nil then
-    wait(5)
     hop()
 end
